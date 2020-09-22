b744664cbcf147a1a93a40c1600fd1874fe964f7
Todd Stoneberg <todd.stoneberg@gmail.com>
2020-09-22 21:57:30 +0000

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,28 @@
 # code here!
+require 'pry'
+
+class School 
+
+  attr_reader :name, :roster
+  
+  def initialize(name)
+    @name = name
+  end
+  
+  def roster
+    roster = {}
+    @roster = roster
+  end
+  
+  def add_student (name, grade)
+    if roster[grade] 
+      roster[grade] << name
+      # @roster = roster[grade]
+    else
+      roster[grade] = [] 
+      roster[grade] << name
+      # @roster = roster[grade]
+    end
+  end
+    
+end
